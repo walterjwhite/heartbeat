fb60b952c1646f88acfed8dbb178aa90ad6a6e9e
Walter White <WalterWhite@walterjwhite.com>
2019-07-15 12:13:20 -0400

    updated/merged

--- src/main/aspect/com/walterjwhite/heartbeat/aspects/HeartbeatAspect.java
+++ src/main/aspect/com/walterjwhite/heartbeat/aspects/HeartbeatAspect.java
@@ -5,13 +5,13 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 
 /**
- * Heartbeat enforcer, let's public methods run for 1s (by default, unless otherwise specified) then
- * interrupts them.
+ * Allows methods to do some sort of "heartbeat" activity while they're running to ensure other
+ * systems know they're still running during long-running tasks.
  */
 @Aspect
 public class HeartbeatAspect {
   @Around(
-          "execution(* *(..)) && @annotation(com.walterjwhite.heartbeat.annotation.Heartbeat)")
+      "execution(* *(..)) && @annotation(com.walterjwhite.heartbeat.annotation.Heartbeat) && !within(com.walterjwhite.heartbeat.*)")
   public Object doHeartbeat(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
     return new HeartbeatInstance(proceedingJoinPoint).call();
   }
